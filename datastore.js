477f9f14653e4c300e3e53c0afef5ba18a17f938
Patrick Gustafson <patrickgus@charter.net>
2019-09-06 10:24:17 -0700

    linked js files to index.html and added content to datastore.js

---
+++ datastore.js
@@ -0,0 +1,102 @@
+const STORE = [
+  {
+    question: 'What is the Millenium Falcon?',
+    answers: [
+      'The origin of the Force',
+      'Han Solo\’s spaceship',
+      'Boba Fett\’s spaceship',
+      'Darth Vader\’s lightsaber'
+      ],
+    correctAnswer: 'Han Solo\’s spaceship'
+  },
+  {
+    question: 'Finish this line: “Do. Or do not. _______.”',
+    answers: [
+      'I am your father',
+      'Try not',
+      'Do it',
+      'There is no try'
+      ],
+    correctAnswer: 'There is no try'
+  },
+  {
+    question: 'What is Darth Vader\’s real name?',
+    answers: [
+      'Ben Skywalker',
+      'Wade Wilson',
+      'Anakin Skywalker',
+      'Qui-Gon Jinn'
+      ],
+    correctAnswer: 'Anakin Skywalker'
+  },
+  {
+    question: 'Who are Kylo Ren\’s parents?',
+    answers: [
+      'Leia Organa and Han Solo',
+      'Anakin Skywalker and Padmé Amidala',
+      'Owen Lars and Beru Whitesun',
+      'Sheev Palpatine and Mon Mothma'
+      ],
+    correctAnswer: 'Leia Organa and Han Solo'
+  },
+  {
+    question: 'How did Boba Fett die?',
+    answers: [
+      'Mace Windu cut off his head',
+      'He fell into the Sarlacc\’s pit',
+      'Han Solo shot him first',
+      'Darth Vader Force choked him'
+      ],
+    correctAnswer: 'He fell into the Sarlacc\’s pit'
+  },
+  {
+    question: 'What was the actual purpose of the clone army in episode 3?',
+    answers: [
+      'To save the galaxy from the Dark Side',
+      'To destroy the droid armies',
+      'To protect the Galactic Senate',
+      'To eliminate the Jedi Order'
+      ],
+    correctAnswer: 'To eliminate the Jedi Order'
+  },
+  {
+    question: 'How many Jedi Masters made up the Jedi High Council?',
+    answers: [
+      '12',
+      '5',
+      '10,000',
+      '100'
+      ],
+    correctAnswer: '12'
+  },
+  {
+    question: 'Where did Rey find Luke Skywalker in Episode 7?',
+    answers: [
+      'At his childhood home on Tatooine',
+      'At the first Jedi Temple on Ahch-To',
+      'With the Ewoks on the forest moon of Endor',
+      'In the remains of the Jedi Temple on Coruscant'
+      ],
+    correctAnswer: 'At the first Jedi Temple on Ahch-To'
+  },
+  {
+    question: 'What classic movie scene was the pod race scene in Episode 1 based on?',
+    answers: [
+      'The Mexican standoff scene in The Good, the Bad, and the Ugly',
+      'The car chase scene in Bullitt',
+      'The chariot race scene in Ben Hur',
+      'The race scene in Death Race 2000'
+      ],
+    correctAnswer: 'The chariot race scene in Ben Hur'
+  },
+  {
+    question: 'Who shot first: Greedo or Han Solo?',
+    answers: [
+      'Obviously Han dummy',
+      'Greedo (you can’t be serious...)',
+      'It’s not this answer',
+      'It’s the first answer'
+      ],
+    correctAnswer: 'Obviously Han dummy'
+  }
+];
