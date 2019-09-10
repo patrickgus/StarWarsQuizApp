2cdb5a3f141a4519fff139e263147bd29d1a0693
Patrick Gustafson <patrickgus@charter.net>
2019-09-10 12:50:39 -0700

    added `handleSubmitButton` function and modified style.css

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function handleStartButton() {
   // Hide .quizStart class
   // replace with next question
   // increase questionNumber
-  console.log('`handleStartButton` ran');
+
   $('.quizStart').on('click', '.startButton', function(event) {
     $('.quizStart').hide();
     
@@ -45,11 +45,48 @@ function handleStartButton() {
   });
 }
 
+function increaseScore() {
+  score ++;
+  $('.score').text(score);
+}
+
+function renderCorrectAnswer() {
+  $('.questionAnswerForm').html(`<h2>Correct!</h2>
+        <button type="button" class="nextButton">Next</button>`);
+  
+  increaseScore();
+}
+
+function renderIncorrectAnswer() {
+  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+
+  $('.questionAnswerForm').html(`<h2>Incorrect!</h2>
+          <p>The correct answer is <span>"${correctAnswer}"</span></p>
+        <button type="button" class="nextButton">Next</button>`);
+} 
+
+function renderFeedback() {
+  let selected = $('input:checked');
+  let answer = selected.val();
+  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+
+  if (answer === correctAnswer) {
+    renderCorrectAnswer();
+  } else {
+    renderIncorrectAnswer();
+  }
+}
+
 function handleSubmitButton() {
   // hide question
   // display feedback correct or incorrect
   // change score if feedback is correct
-  console.log('`handleSubmitButton` ran');
+
+  $('.questionAnswerForm').on('click', '.submitButton', function(event) {
+    event.preventDefault();
+
+    renderFeedback();
+  });
 }
 
 function handleNextButton() {
@@ -57,14 +94,12 @@ function handleNextButton() {
   // display next question (unless last question)
   // increase question number
   // after last question dispay results
-  console.log('`handleNextButton` ran');
 }
 
 function handleRestartButton() {
   // hide results
   // display start screen
   // change score and question numbers to 0
-  console.log('`handleRestartButton` ran');
 }
 
 function handleButtons() {
